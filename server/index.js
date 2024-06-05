38d4628f0e3d200c3b1d3c4ff9cd4b8dd50d1674
vikassharma011 <vikassharma9669@gmail.com>
2024-06-06 00:57:03 +0530

    fl ot

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ import Jwt from "jsonwebtoken";
 import path from "path";
 import { fileURLToPath } from 'url';
 import dbConnect from "./utilis/db.js";
-import { fileURLToPath } from 'url';
+
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
